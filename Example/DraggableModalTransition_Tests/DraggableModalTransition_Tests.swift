589c340694706275999915f623d6af70bb084849
shingt <shingtshin@gmail.com>
2017-07-26 08:36:26 +0900

    Add test target for bitrise

---
+++ Example/DraggableModalTransition_Tests/DraggableModalTransition_Tests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+class DraggableModalTransition_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+}
